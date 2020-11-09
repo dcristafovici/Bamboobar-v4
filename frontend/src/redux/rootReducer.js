713dbbde917e72035d68ad6fcb86ae81bb69a6e2
denibudeyko <denbudeyko@gmail.com>
2020-11-09 10:45:54 +0200

    fix to add

---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+  asideItems: {}
+}
+const rootReducer = (state = initialState, action) =>{
+  return state
+}
+
+export default rootReducer
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express()
 
 app.use(express.json())
 app.use(fileupload())
-app.use(express.static('./uploads/'));
 app.use(cors())
+app.use("/", express.static(__dirname));
 
 // api/NameController
 app.use('/api/product', require('./routes/product'))
