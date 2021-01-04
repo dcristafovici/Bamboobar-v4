2ddd460f793085612d4903f951addc15e959f622
denibudeyko <denbudeyko@gmail.com>
2021-01-04 07:23:09 +0200

    Èbefore run kladr-api

--- frontend/src/components/find/Find.js
+++ frontend/src/components/find/Find.js
@@ -1,28 +1,9 @@
 import React, {useState} from "react"
-import axios from 'axios'
-import { YMaps, Map } from "react-yandex-maps";
 const Find = () => {
-  const [address,setAddress] = useState({
-    address: ''
-  })
-
-  const loadSuggest = ymaps => {
-    const suggestView = new ymaps.SuggestView("suggest");
-    console.log(suggestView)
-  };
-
-
   return(
     <div className="banner-find">
       <h1>Быстрая Доставка из лучшего ресторана<br/>в москва-СИТИ bamboo.bar</h1>
-      {/*<YMaps>*/}
-      {/*  <Map*/}
-      {/*    onLoad={ymaps => loadSuggest(ymaps)}*/}
-      {/*    defaultState={{ center: [55.751574, 37.573856],  zoom: 9 }}*/}
-      {/*    modules={["SuggestView"]}*/}
-      {/*  />*/}
-      {/*  <input type="text" id='suggest' onChange={ymaps => console.log(ymaps)}  />*/}
-      {/*</YMaps>*/}
+
 
       <div className="form-group ">
         <div className="form-group__clear">
