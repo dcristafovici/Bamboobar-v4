fcc4cc9f2f5f01381a9f39b556ae6ba0be1f8a43
denibudeyko <denbudeyko@gmail.com>
2021-01-26 11:34:01 +0200

    integration with sberbank

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const {Order, CartItem} =  require('../models/order.models')
-
+const qs = require('qs')
 const createOrder = async (req, res) => {
   try{
     const data = req.body
@@ -25,18 +25,25 @@ const getOrderByUser = async(req, res) => {
 const payOrder = async(req, res) => {
   try{
     const { amount, id, products } = req.body
-
-    const payment = await axios.post(
-      'https://3dsec.sberbank.ru/payment/rest/register.do',
-      {
+    axios({
+      method: "POST",
+      url: "https://3dsec.sberbank.ru/payment/rest/register.do",
+      data: qs.stringify({
         userName: "delivery-bamboobar-api",
         password: ">@^z-J8XW#'-26~[",
-        orderId: "231"
-      },
-
-
-    )
-    console.log(payment.data)
+        returnUrl: 'http://google.com/',
+        failUrl: 'http://google.com/',
+        orderId: 240,
+        orderNumber: 150,
+        amount: 6000
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      }
+    })
+      .then(response => {
+        console.log(response)
+      })
 
   } catch (err){
     res.status(500).json({error: err.message})
