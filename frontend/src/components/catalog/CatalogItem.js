25997792fa84c5dc8531526f5af9554b96915d75
denibudeyko <denbudeyko@gmail.com>
2021-02-08 11:44:37 +0200

    Final refactoring

--- frontend/src/components/catalog/CatalogItem.js
+++ frontend/src/components/catalog/CatalogItem.js
@@ -1,12 +1,17 @@
 import React from "react"
+import {useSelector, connect} from "react-redux";
+import {addToCart} from "../../redux/actions/asideAction";
+import {togglePopup} from '../../redux/actions/addressAction'
 
+const CatalogItem = ({products, addToCart, togglePopup}) => {
 
-const CatalogItem = ({products}) => {
-
+  const addressReducer = useSelector(state => state.addressReducer)
+  const {delivery} = addressReducer
   return (
     products.map((product, index) => {
       return (
-        <div key={index} className="catalog-item">
+        <div key={index} className="catalog-item"
+             onClick={() => (delivery) ? addToCart(product) : togglePopup()}>
           <div className="catalog-item__top">
             <h4>{product.name}</h4>
             <span><span className="woocommerce-Price-amount amount">
@@ -14,7 +19,7 @@ const CatalogItem = ({products}) => {
                 <span className="woocommerce-Price-currencySymbol">₽</span></bdi></span></span>
           </div>
           <div className="catalog-item__photo">
-            <img src={"http://localhost:5000/" + product.productImage} />
+            <img src={"http://localhost:5000/" + product.productImage} alt={product.name}/>
             <div className="catalog-photo__general">
               <div className="catalog-photo__gramme"><span>{product.weight} г</span>
               </div>
@@ -33,5 +38,15 @@ const CatalogItem = ({products}) => {
 
 }
 
+const mapStateToProps = state => {
+  return state
+}
 
-export default CatalogItem
+const mapDispatchToProps = dispatch => {
+  return{
+    addToCart: (product) => dispatch(addToCart(product)),
+    togglePopup: () => dispatch(togglePopup())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CatalogItem)
