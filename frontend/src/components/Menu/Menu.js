08a3d724612f04ed661f3c5a4eb7b396861f9b80
denibudeyko <denbudeyko@gmail.com>
2021-02-18 08:38:49 +0200

    full app

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -4,21 +4,52 @@ import {useDispatch, useSelector} from "react-redux";
 const Menu = () => {
 
   const [scroll, setScroll] = useState(false)
-
-  useEffect(() => {
-    window.addEventListener('scroll', function (event){
-      setScroll(window.scrollY > 172);
-    })
-  },[])
-
-  useEffect(() => {
-    const element = document.getElementById('banner')
-    const domRect = element.getBoundingClientRect()
-  }, [])
-
+  const [positions, setPositions] = useState([])
+  const [bannerPosition, setBannerPosition] = useState(0)
 
   const categoriesReducer = useSelector(state => state.categoriesReducer)
   const{ loading , errors, categories } = categoriesReducer
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', function (event){
+      setScroll(window.scrollY > bannerPosition);
+      if (window.scrollY < positions[0]) return;
+      const index = positions.findIndex((el, i, arr) => window.scrollY < arr[i + 1]);
+
+      const categoryAttribute = "[data-id=" + "'" + index + "'" + "]";
+      const element = document.querySelector(categoryAttribute)
+
+      const allElements = document.querySelectorAll('.menu-nav li span')
+      for(let el of allElements){
+        el.classList.remove('active')
+      }
+
+      if(element) element.classList.add('active')
+      if(index === -1 && window.scrollY > positions[0]){
+        allElements[allElements.length - 1].classList.add('active')
+      }
+    })
+
+  },[bannerPosition, positions])
+
+  useEffect(() => {
+    const element = document.getElementById('banner')
+    const domRect = element.getBoundingClientRect().top + window.pageYOffset
+    setBannerPosition(domRect)
+  }, [])
+
+
+  useEffect( () => {
+    const allPoints = document.getElementsByClassName('catalog-point')
+    let allPositions = []
+    for(let point of allPoints){
+      const position = point.getBoundingClientRect().top + window.pageYOffset - 60
+      allPositions.push(position)
+    }
+    setPositions(allPositions)
+  }, [categories])
+
 
   const goToCategory = (index) => {
     const categoryAttribute = "[data-category=" + "'" + index + "'" + "]";
@@ -27,6 +58,8 @@ const Menu = () => {
     const y = selector.getBoundingClientRect().top + window.pageYOffset + yOffset
     window.scrollTo({top: y, behavior: "smooth"})
   }
+
+
 
   return(
     <section className={"menu" + (scroll ? " menu-fixed" : "")} id="menu">
