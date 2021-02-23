859369720481c6ca1f90ac4050b014d80c41ed48
denibudeyko <denbudeyko@gmail.com>
2021-02-23 07:35:48 +0200

    a small modifications for server

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 const path = require('path')
 const axios = require('axios')
 const app = express()
+require('dotenv').config()
+
+
 
 app.use(express.json())
 app.use(fileupload())
@@ -26,7 +29,14 @@ app.use('/api/order', require('./routes/order.routes'))
 app.use('/api/address', require('./routes/data.routes'))
 app.use('/api/find', require('./routes/find.routes'))
 
-const PORT = config.get('port') || 5000
+
+if(process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+const PORT = process.env.PORT || 5000
 const start = async () =>{
   try{
     await mongoose.connect(
